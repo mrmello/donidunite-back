e3e9e0f55009cd3f6616b09108f7f3a8fc0fdbf6
mrmello <mrmello92@hotmail.com>
2018-03-10 14:13:26 -0300

    adding cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors')
 var catalog = require('./routes/catalog');
 var users = require('./routes/users');
 var management = require('./routes/management');
@@ -18,6 +19,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
 app.use('/auth', auth);
 app.use('/management', management);
 app.use('/users', users);
