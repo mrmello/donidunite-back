937c94ecb6f74831f50be3735affdd6ad67d2182
Marcos Mello <marcos.mello@compasso.com.br>
2018-06-20 20:53:04 -0300

    adding expense update and delete

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -5,14 +5,14 @@ const Expense = mongoose.model('Expense');
 
 exports.expense_list = function(req, res) {
   Expense.find()
-    .populate('category')
-    .populate('payment')
-    .then(resp => {res.send(resp)})
-    .catch(err => {res.send(err)});
+  .populate('category')
+  .populate('payment')
+  .then(resp => {res.send(resp)})
+  .catch(err => ( sendErrors(res, err) ));
 };
 
 exports.expense_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: expense detail: ' + req.params.id);
+  res.send('NOT IMPLEMENTED: expense detail: ' + req.params.id);
 };
 
 exports.expense_create = function(req, res) {
@@ -26,14 +26,31 @@ exports.expense_create = function(req, res) {
     toWho:        req.body.toWho,
   });
   expense.save()
-    .then(resp => { res.send(resp) })
-    .catch(err => ( sendErrors(res, err) ));
+  .then(resp => { res.send(resp) })
+  .catch(err => ( sendErrors(res, err) ));
 };
 
 exports.expense_delete = function(req, res) {
-    res.send('NOT IMPLEMENTED: expense delete GET');
+  Expense.remove({ _id: req.params.id })
+  .then(resp => { res.send(resp) })
+  .catch(err => ( sendErrors(res, err) ));
 };
 
 exports.expense_update = function(req, res) {
-    res.send('NOT IMPLEMENTED: expense update POST');
+  Expense.findById({ _id: req.params.id })
+  .then(expense => {
+    expense.set({
+      description:  req.body.description,
+      value:        req.body.value,
+      category:     req.body.category,
+      payment:      req.body.payment,
+      payee:        req.body.payee,
+      date:         req.body.date,
+      toWho:        req.body.toWho
+    })
+    expense.save()
+    .then(resp => { res.send(resp) })
+    .catch(err => ( sendErrors(res, err) ));
+  })
+  .catch(err => ( sendErrors(res, err) ));
 };
