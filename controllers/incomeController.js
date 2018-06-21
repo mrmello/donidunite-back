47b360484655dc3ca30b7ad62c41877bbfc16189
Marcos Mello <marcos.mello@compasso.com.br>
2018-06-20 21:36:14 -0300

    adding delete and edit incomes

--- controllers/incomeController.js
+++ controllers/incomeController.js
@@ -5,14 +5,14 @@ const { sendErrors } = require('./handleErrors')
 
 exports.income_list = function(req, res) {
   Income.find()
-    .populate('category')
-    .populate('payment')
-    .then(resp => {res.send(resp)})
-    .catch(err => {res.send(err)});
+  .populate('category')
+  .populate('payment')
+  .then(resp => {res.send(resp)})
+  .catch(err => {res.send(err)});
 };
 
 exports.income_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: income detail: ' + req.params.id);
+  res.send('NOT IMPLEMENTED: income detail: ' + req.params.id);
 };
 
 exports.income_create = function(req, res) {
@@ -25,14 +25,30 @@ exports.income_create = function(req, res) {
     date:         req.body.date
   });
   income.save()
-    .then(resp => { res.send(resp) })
-    .catch(err => ( sendErrors(res, err) ));
+  .then(resp => { res.send(resp) })
+  .catch(err => ( sendErrors(res, err) ));
 };
 
 exports.income_delete = function(req, res) {
-    res.send('NOT IMPLEMENTED: income delete GET');
+  Income.remove({ _id: req.params.id })
+  .then(resp => { res.send(resp) })
+  .catch(err => ( sendErrors(res, err) ));
 };
 
 exports.income_update = function(req, res) {
-    res.send('NOT IMPLEMENTED: income update POST');
+  Income.findById({ _id: req.params.id })
+  .then(income => {
+    income.set({
+      description:  req.body.description,
+      value:        req.body.value,
+      category:     req.body.category,
+      payment:      req.body.payment,
+      payee:        req.body.payee,
+      date:         req.body.date
+    })
+    income.save()
+    .then(resp => { res.send(resp) })
+    .catch(err => ( sendErrors(res, err) ));
+  })
+  .catch(err => ( sendErrors(res, err) ));
 };
